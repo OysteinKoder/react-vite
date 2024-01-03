a47622d5bafef39fa8929d38d4a7fa49afc3c8fd
OysteinKoder <oystein.kodehode@gmail.com>
2024-01-03 14:42:57 +0100

    counter app task using useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,19 @@
 import "./App.css";
-// Import your components here
+// Import useState here.
 
 function App() {
-  return <>{/* Use your three different components here */}</>;
+  // Declare a new useState variable called count here
+
+  // Make a function called incrementCount here, and use it to,,, you guessed it increment the count variable
+
+  // Make a function called decrementCount here, and use it to,,, you guessed it decrement the count variable
+  return (
+    <>
+      {/* Add a button that calls the incrementCount function when clicked */
+      /* Add a button that calls the decrementCount function when clicked */
+      /* Display the count variable in a h1 tag */}
+    </>
+  );
 }
 
 export default App;
