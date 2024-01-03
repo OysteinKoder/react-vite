d79f1ed9e7034f49a1ab9c28f95354bc5c444bf9
OysteinKoder <oystein.kodehode@gmail.com>
2024-01-03 15:08:39 +0100

    react router task

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,9 @@
 import "./App.css";
-// Import useState here.
+// Import the react router components here
+// Import your different pages here
 
 function App() {
-  // Declare a new useState variable called count here
-
-  // Make a function called incrementCount here, and use it to,,, you guessed it increment the count variable
-
-  // Make a function called decrementCount here, and use it to,,, you guessed it decrement the count variable
-  return (
-    <>
-      {/* Add a button that calls the incrementCount function when clicked */
-      /* Add a button that calls the decrementCount function when clicked */
-      /* Display the count variable in a h1 tag */}
-    </>
-  );
+  return <>{/* {make the router here} */}</>;
 }
 
 export default App;
